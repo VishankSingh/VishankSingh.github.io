7cc1ca9943265ed3e80889b1282faf123bfe801a
VishankSingh <vishanksinghh@gmail.com>
2020-10-08 13:12:55 +0530

    site

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ particlesJS('particles-js',
     {
         "particles": {
             "number": {
-                "value": 60,
+                "value": 40,
                 "density": {
                     "enable": true,
                     "value_area": 800
@@ -56,7 +56,7 @@ particlesJS('particles-js',
             },
             "move": {
                 "enable": true,
-                "speed": 4,
+                "speed": 3,
                 "direction": "none",
                 "random": false,
                 "straight": false,
