905a45f4dd0ea1f5dea76b9f381bbfc70a1aa07a
vishanksingh <vishanksinghh@gmail.com>
2024-09-21 23:47:18 +0530

    modified routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home.jsx';
 import Blog from './pages/Blog/Blog.jsx';
 import Feed from './pages/Feed/Feed.jsx';
 import Projects from './pages/Projects/Projects.jsx';
-import {useEffect} from 'react';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/blog" element={<Blog />}></Route>
-          <Route path="/feed" element={<Feed />}></Route>
-          <Route path="/projects" element={<Projects />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
